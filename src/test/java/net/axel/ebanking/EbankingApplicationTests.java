6b1842e46bea91c9fba5f46556427ef4d0a4b590
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-02 12:58:51 +0100

    fc

---
+++ src/test/java/net/axel/ebanking/EbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package net.axel.ebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
