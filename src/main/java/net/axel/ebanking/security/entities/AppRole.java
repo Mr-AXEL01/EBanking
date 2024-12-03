74c5644cf1231c776e461322c7c6457a84edc8ac
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-04 00:55:33 +0100

    add AppRole entity
    set AppRole entity for the roles that we gonna use.

---
+++ src/main/java/net/axel/ebanking/security/entities/AppRole.java
@@ -0,0 +1,28 @@
+package net.axel.ebanking.security.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "roles")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true, fluent = true)
+public class AppRole implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roleName;
+
+    public AppRole(String roleUser) {
+    }
+}
