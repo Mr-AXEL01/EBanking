dd9cfa38590386f509e04777345b9431a52fcaae
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-04 10:34:55 +0100

    change the return type for findByName()
    update the return type to be Optional

--- src/main/java/net/axel/ebanking/security/repository/RoleRepository.java
+++ src/main/java/net/axel/ebanking/security/repository/RoleRepository.java
@@ -4,5 +4,9 @@ import net.axel.ebanking.security.entities.AppRole;
 import net.axel.ebanking.security.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<AppRole, Long> {
+
+    Optional<AppRole> findByName(String name);
 }
