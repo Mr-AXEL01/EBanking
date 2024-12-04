b5fd3b5f3b57bea4fd5b4cbab81b7c3b0496e412
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-04 11:05:31 +0100

    add controller for manage user
    set a controller to manage user by define the endpoints that we gonna use.

---
+++ src/main/java/net/axel/ebanking/security/controller/UserController.java
@@ -0,0 +1,37 @@
+package net.axel.ebanking.security.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import net.axel.ebanking.security.dtos.user.UserRequestDTO;
+import net.axel.ebanking.security.dtos.user.UserResponseDTO;
+import net.axel.ebanking.security.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(UserController.CONTROLLER_PATH)
+
+@RequiredArgsConstructor
+public class UserController {
+
+    public final static String CONTROLLER_PATH = "/api/users";
+
+    private final UserService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> register(@RequestBody @Valid UserRequestDTO dto) {
+        UserResponseDTO user = service.createUser(dto);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> findAll() {
+        List<UserResponseDTO> users = service.findUsers();
+        return ResponseEntity.ok(users);
+    }
+
+
+}
