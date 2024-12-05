7e9a49663355f33978de2bcad612e68dfc44a00e
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-05 23:33:23 +0100

    add controller for public endpoints

---
+++ src/main/java/net/axel/ebanking/controller/RegisterController.java
@@ -0,0 +1,26 @@
+package net.axel.ebanking.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import net.axel.ebanking.security.dtos.user.UserRequestDTO;
+import net.axel.ebanking.security.dtos.user.UserResponseDTO;
+import net.axel.ebanking.security.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/public")
+
+@RequiredArgsConstructor
+public class RegisterController {
+
+    private final UserService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> register(@RequestBody @Valid UserRequestDTO dto) {
+        UserResponseDTO user = service.createUser(dto);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+}
+
