209666b0c94ea7d6b65663f710f6af639caab0a7
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-06 21:30:32 +0100

    set config security

--- src/main/java/net/axel/ebanking/security/config/securityConfig.java
+++ src/main/java/net/axel/ebanking/security/config/securityConfig.java
@@ -1,15 +1,79 @@
 package net.axel.ebanking.security.config;
 
+import lombok.RequiredArgsConstructor;
+import net.axel.ebanking.security.entities.AppUser;
+import net.axel.ebanking.security.providers.CustomAuthenticationProvider;
+import net.axel.ebanking.security.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class securityConfig {
+    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .authenticationProvider(customProvider())
+                .build();
+    }
 
     @Bean
-    PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/public/**").permitAll()
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/user/**").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .userDetailsService(userDetailsService());
+
+        return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            AppUser user = userService.findUserEntity(username);
+
+            String roleWithPrefix = "ROLE_" + user.getRole().getName();
+            List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(roleWithPrefix));
+            System.out.println("The Role of your user is : " + user.getRole().getName());
+            return new User(
+                   user.getUsername(),
+                   user.getPassword(),
+                   authorities
+            );
+        };
+    }
+
+    @Bean
+    public AuthenticationProvider customProvider() {
+        return new CustomAuthenticationProvider(userDetailsService(), passwordEncoder);
+    }
+
 }
