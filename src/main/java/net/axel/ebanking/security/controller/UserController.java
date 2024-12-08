94bd4beb9229a13b730cb57d96150de3d7796e11
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-08 02:21:54 +0100

    set new endPoints
    implement the banking endpoints in the user controller.

--- src/main/java/net/axel/ebanking/security/controller/UserController.java
+++ src/main/java/net/axel/ebanking/security/controller/UserController.java
@@ -6,10 +6,7 @@ import net.axel.ebanking.security.dtos.user.UserResponseDTO;
 import net.axel.ebanking.security.dtos.user.UserUpdatePasswordDTO;
 import net.axel.ebanking.security.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(UserController.CONTROLLER_PATH)
@@ -17,13 +14,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    public final static String CONTROLLER_PATH = "/api/user/users";
+    public final static String CONTROLLER_PATH = "/api/user";
 
     private final UserService service;
 
-    @PutMapping
+    @PutMapping("/myProfile/password")
     public ResponseEntity<UserResponseDTO> updatePassword(@RequestBody @Valid UserUpdatePasswordDTO dto) {
         UserResponseDTO user = service.updatePassword(dto);
         return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/myLoans")
+    public String getLoans() {
+        return "Your loans information";
+    }
+
+    @GetMapping("/myCards")
+    public String getCards() {
+        return "Your cards information";
+    }
+
+    @GetMapping("/myAccount")
+    public String getAccount() {
+        return "Your account information";
+    }
+
+    @GetMapping("/myBalance")
+    public String getBalance() {
+        return "Your balance information";
     }
 }
